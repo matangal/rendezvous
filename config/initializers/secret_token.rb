100385dd61bf8ba60ba67730c46598d0fbd2f883
Matan Gal <matangal123@gmail.com>
2012-08-28 14:22:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rendezvous::Application.config.secret_token = '4803cc2cbaa712c48eedf433a1072aae309a66a4b94be9a03a9fb96a22ae4f59504526d2a7380a0d51e344dc8c5a0c356b21042ff5463988dbe6d090da485573'
